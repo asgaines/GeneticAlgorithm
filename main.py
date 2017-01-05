807c3b73e89d7938f5f09ddf4d1d3927bc6a9cf3
asgaines <andrew.s.gaines@gmail.com>
2017-01-05 12:40:12 -0700

    Move population initialization in main function

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import config
 from organism import Organism
 from population import Population
 
-population = Population(
-    target=config.values['target'],
-    num_organisms=config.values['num_population']
-)
 
 if __name__ == '__main__':
+    start = datetime.now()
+
+    population = Population(
+        target=config.values['target'],
+        num_organisms=config.values['num_population']
+    )
+
     while not population.target_achieved():
         population.generation()
         # Uncomment the following to see the results in real time
